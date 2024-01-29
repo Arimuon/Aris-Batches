3e7f789a57c62fdc0d712a470eaf8877144b01fe
Ari <144230403+Arimuon@users.noreply.github.com>
2024-01-29 14:34:08 +0000

    Update PreferenceBatch.bat

--- PreferenceBatch.bat
+++ PreferenceBatch.bat
@@ -1,34 +1,51 @@
 @ECHO OFF
 ECHO Preference Automation
 :: Here is the settings for this file
-set RunOnStartup = False :: Change to "True" if you want to run this on startup
-set ChangeDownloadFolder = False :: Change to "True" if you want to change Chrome's Download location
+set RunOnStartup = False :: Off by default, change to "True" if you want to run this on startup
+set ChangeDownloadFolder = False :: Off by default, change to "True" if you want to change Chrome's Download location
+set ChangeDefaultBrowser = True :: On by default, change to "False" if you want to don't want to change the default browser to Chrome
+set ChangePersonalisation = True :: On by default, change to "False" if you don't want to change the personalisation settings
 
-:: Sets apps to dark theme
-REG add "HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v AppsUseLightTheme /t REG_DWORD /d 0 /F
-:: Sets system to dark theme
-REG add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v SystemUsesLightTheme /t REG_DWORD /d 0 /F
-REG add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v ColorPrevalence /t REG_DWORD /d 0 /F
-:: On Windows 11 Alligns taskbar to the left
-REG add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v TaskbarAl /t REG_DWORD /d 0 /F
-:: Shows file extentions
-REG add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v HideFileExt /t REG_DWORD /d 0 /F
-:: Enables file check boxes
-REG add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v AutoCheckSelect /t REG_DWORD /d 1 /F
-:: Disables menu accent colors
-REG add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Accent" /v AccentColorMenu /d 0 /F
-:: Shows no color on start/taskbar
-REG add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v ShowColorOnStartAndTaskbar /t REG_DWORD /d 0 /F
-:: Disables taskbar transparency
-REG add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v EnableTransparency /t REG_DWORD /d 0 /F
-ECHO Applied Personalisation Settings
+IF "%ChangePersonalisation%"=="True" (
+    ECHO Applying Personalisation Settings
+    :: Sets apps to dark theme
+    REG add "HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v AppsUseLightTheme /t REG_DWORD /d 0 /F
+    :: Sets system to dark theme
+    REG add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v SystemUsesLightTheme /t REG_DWORD /d 0 /F
+    REG add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v ColorPrevalence /t REG_DWORD /d 0 /F
+    :: On Windows 11 Alligns taskbar to the left
+    REG add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v TaskbarAl /t REG_DWORD /d 0 /F
+    :: Shows file extentions
+    REG add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v HideFileExt /t REG_DWORD /d 0 /F
+    :: Enables file check boxes
+    REG add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v AutoCheckSelect /t REG_DWORD /d 1 /F
+    :: Disables menu accent colors
+    REG add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Accent" /v AccentColorMenu /d 0 /F
+    :: Shows no color on start/taskbar
+    REG add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v ShowColorOnStartAndTaskbar /t REG_DWORD /d 0 /F
+    :: Disables taskbar transparency
+    REG add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v EnableTransparency /t REG_DWORD /d 0 /F
+    ECHO Applied Personalisation Settings
+    ) ELSE (
+    ECHO Change personalisation skipped!
+)
 
-ECHO Modifying Registry for Default Browser
-:: Makes Chrome the default browser
-REG add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\FileAssoc.html\shell\open\command" /v "(Default)" /t reg_sz /d "C:\Program Files\Google\Chrome\Application\chrome.exe" /F
-ECHO Chrome Made Default Browser
-ECHO Applying Chrome Settings
-REG add "HKCU\Software\Google\Chrome\Default" /v DownloadDirectory /t REG_SZ /d "C:\Users\%USERPROFILE%\OneDrive - Cheshire College South & West\Downloads" /F
+IF "%ChangeDefaultBrowser%"=="True" (
+    ECHO Modifying Registry for Default Browser
+    :: Makes Chrome the default browser
+    REG add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\FileAssoc.html\shell\open\command" /v "(Default)" /t reg_sz /d "C:\Program Files\Google\Chrome\Application\chrome.exe" /F
+    ECHO Chrome Made Default Browser
+    ) ELSE (
+    ECHO Change default browser skipped!
+)
+
+IF "%ChangeDownloadFolder%"=="True" (
+    ECHO Changing Chrome Download Folder
+    REG add "HKCU\Software\Google\Chrome\Default" /v DownloadDirectory /t REG_SZ /d "C:\Users\%USERPROFILE%\OneDrive - Cheshire College South & West\Downloads" /F
+    ECHO Chrome Download Folder Changed
+) ELSE (
+    ECHO Change download folder skipped!
+)
 
 ECHO Opening Bitwarden Extension
 start chrome https://chromewebstore.google.com/detail/bitwarden-free-password-m/nngceckbapebfimnlniiiahkandclblb
@@ -45,4 +62,4 @@ IF "%RunOnStartup%"=="True" (
 ) ELSE (
     ECHO Run on startup skipped!
 )
-BREAK
+PAUSE
